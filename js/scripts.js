4eade808535508ea84f53b1502b0f1e0513cb3be
Chad McKee <chadm_@yahoo.com>
2017-05-23 15:55:01 -0400

    ticker updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,47 @@
 
 
 $(document).ready(function () {
+
+    const weatherAPI = 'http://api.openweathermap.org/data/2.5/weather';
+
+    var date = new Date(1485799200*1000);
+    console.log(date.toJSON());
+    var call =  'http://api.openweathermap.org/data/2.5/forecast?lat=33.449339&lon=-83.26209' + '&units=imperial&appid=' + apiKey;
+    console.log(call);
+
+    console.log(date.getTimezoneOffset());
+
+    $.getJSON(call, function (data) {
+        console.log(data);
+    });
+
+
+    $('#weather-form').submit(function () {
+        event.preventDefault();
+        var zipCode = $('#zip-code').val();
+
+        weatherURL = weatherAPI + '?zip=' + zipCode + ',us&units=imperial&appid=' + apiKey;
+        //console.log(weatherURL);
+
+        $.getJSON(weatherURL, function (weatherData) {
+            //console.log(weatherData);
+
+            var currentTemp = {currTemp: weatherData.main.temp,
+            max: weatherData.main.temp_max, min: weatherData.main.temp_min};
+
+            var name = weatherData.name;
+            var icon = weatherData.weather[0].icon + '.png';
+            var desc = weatherData.weather[0].description;
+            var newHTML = '<img src="http://openweathermap.org/img/w/' + icon + '">' + desc;
+            newHTML += '<div>The temp in ' + name + ' is currently ' + currentTemp.currTemp + '&deg;</div>';
+            $('#temp-info').html(newHTML);
+            animateCircle(0, currentTemp.currTemp);
+
+        })
+    });
+
+
+
     var canvas = $('#weather-canvas');
     var context = canvas[0].getContext('2d');
 
@@ -8,8 +49,7 @@ $(document).ready(function () {
 
     var currentPercent = 0;
 
-    function animateCircle(currentArc){
-        console.log(currentArc);
+    function animateCircle(currentArc, currentTemp){
         context.fillStyle = '#ccc';
         context.beginPath();
         context.arc(155,75,70,0,Math.PI*2);
@@ -23,9 +63,9 @@ $(document).ready(function () {
         context.stroke();
 
         currentPercent++;
-        if(currentPercent < assumedTemperature){
+        if(currentPercent < currentTemp){
             requestAnimationFrame(function(){
-                animateCircle(currentPercent/100);
+                animateCircle(currentPercent/100, currentTemp);
             });
 
         }
